3c684c1c62d9f758d2664066c07b512a0f93ce1e
Scott Bowen <scottbowen@EKN-OSX100.local>
2020-02-14 12:03:39 -0500

    added initial VC and mapView

---
+++ peoplemontng/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  peoplemontng
+//
+//  Created by Scott Bowen on 2/14/20.
+//  Copyright © 2020 Scott Bowen. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
