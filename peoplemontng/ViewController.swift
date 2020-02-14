6d803d6ae11483f7d49a8c270482d14b492c903f
Scott Bowen <scottbowen@EKN-OSX100.local>
2020-02-14 11:45:11 -0500

    init commit

---
+++ peoplemontng/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  peoplemontng
+//
+//  Created by Scott Bowen on 2/14/20.
+//  Copyright © 2020 Scott Bowen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
